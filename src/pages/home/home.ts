0a2168ea8ee569df4f2d7cd45b0f6949295d79b1
Guillaume Soulié <gsoulie@isimedia.com>
2018-12-21 09:44:48 +0100

    ajout lazy loading

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,8 +2,8 @@ import { AngularFireList, AngularFireDatabase } from 'angularfire2/database';
 import { MatchDetailPage } from './../match-detail/match-detail';
 import { MatchNewPage } from './../match-new/match-new';
 import { DataProvider } from '../../providers/data/data';
-import { Component } from '@angular/core';
-import { NavController, AlertController, LoadingController } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { NavController, AlertController, LoadingController, Content } from 'ionic-angular';
 import { Observable } from 'rxjs';
 import { cst } from '../../models/constantes';
 
@@ -18,6 +18,14 @@ export class HomePage {
   deckList: AngularFireList<any>;
   deckObservable: Observable<any[]>;
   decks: any[] = [];
+  matchs2: any[] = [];
+  selectedDeck;
+
+  @ViewChild(Content) content: Content; // ion-content 
+  private direction: string = ""; // to get scroll direction
+  private lastScrollTop: number = 0;
+  private lastItem = null;
+  private filteringMode = false;
 
   constructor(public navCtrl: NavController,
     private dataService: DataProvider,
@@ -27,7 +35,39 @@ export class HomePage {
       this.getMatchs();
   }
 
+  ngAfterViewInit() {
+    // Add scroll listener
+    this.content.ionScrollEnd.subscribe((data) => {
+      let currentScrollTop = data.scrollTop;
+      if(currentScrollTop > this.lastScrollTop){
+        this.direction = 'down';
+      }else if(currentScrollTop < this.lastScrollTop){
+        this.direction = 'up';
+      }
+      this.lastScrollTop = currentScrollTop;
+    });
+  }
+
+  /**
+   * Infinite scroll listener
+   * @param infiniteScroll 
+   */
+  scrollHandler(infiniteScroll){
+    setTimeout(() => {
+      // Update data only when scrolling down 
+      if(this.direction == 'down'){
+        // Update data only when end of page is detected
+        if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
+          this.getMatchs();
+        }
+      }
+      infiniteScroll.complete();
+    }, 500);    
+  }
+
   getMatchs(){
+    console.log("--- fetching all match ----");
+    if(this.filteringMode == false){
     this.dataList = this.afDB.list(cst.TBL_DECK, ref => ref.orderByChild('deckName'));
     this.deckObservable = this.dataList.valueChanges();
     this.deckObservable.subscribe((res) => {
@@ -39,16 +79,30 @@ export class HomePage {
       content: "Chargement des parties..."
     });
     loader.present();
-    this.dataList = this.afDB.list(cst.TBL_MATCH, ref => ref.orderByChild('datetime'));
+
+    if(this.lastItem === null){
+      this.dataList = this.afDB.list(cst.TBL_MATCH, ref => ref.orderByChild('id').limitToLast(20));
+    } else {
+      this.dataList = this.afDB.list(cst.TBL_MATCH, ref => ref.orderByChild('id').endAt(this.lastItem).limitToLast(20));
+    }
     this.matchs = this.dataList.valueChanges().map(items => items.sort().reverse());
     try{
       this.matchs.subscribe((res) => {
+          let temp = res as any[];
+          this.lastItem = temp[temp.length-1].id;
+
+          if(this.matchs2.length > 0){
+            temp.splice(0,1);
+          }
+          temp.forEach(match => {
+            this.matchs2.push(match);
+          });
           loader.dismiss();        
       })
     } catch(e){
       loader.dismiss();
     }
-    
+    }
   }
 
   filteringItem(ev){
@@ -57,16 +111,29 @@ export class HomePage {
 
     // Avoid research if searchtext is empty
     if (!searchText || searchText.trim() === '' || searchText === 'Tous') {
-      this.matchs = this.dataList.valueChanges().map(items => items.sort().reverse());
+      this.filteringMode = false;
+      //this.matchs = this.dataList.valueChanges().map(items => items.sort().reverse());
+      this.lastItem = null;
+      this.matchs2 = [];
+      this.getMatchs();
+      console.log("--- modeFiltering --- " + this.filteringMode);
       return;
+    } else {
+      this.filteringMode = true;
+      console.log("--- modeFiltering --- " + this.filteringMode);
     }
     
+    this.dataList = this.afDB.list(cst.TBL_MATCH, ref => ref.orderByChild('id'));
     this.matchs = this.dataList.valueChanges().map(items => items.filter((match) => {      
       if ((match.deck1.deckName.toLowerCase().indexOf(searchText.toLowerCase()) > -1) || (match.deck2.deckName.toLowerCase().indexOf(searchText.toLowerCase()) > -1)) {
         return true;
       }
       return false;
     }));
+    this.matchs.subscribe((res) => {
+      this.matchs2 = res as any[];
+      this.lastItem = null;
+    })
   }
 
   onAddNewMatch(){
