031fddbf38dc49b0043e4064449de3dffed7b2d6
Guillaume Soulié <gsoulie@isimedia.com>
2019-04-12 17:13:54 +0200

    ajout pourcentage des maisons

--- src/pages/statistic/statistic.ts
+++ src/pages/statistic/statistic.ts
@@ -18,24 +18,34 @@ export class StatisticPage implements OnInit{
   comboDeck: any[] = [];
   stats: any[];
   selectedPlayer: string = "";
+  filteringInProcess = false;
+
+  factionWinRating = [];
+  factionDisObject: any = {selected: false, faction: cst.FACTION_DIS, nb: 0};
+  factionBrobnarObject: any = {selected: false, faction: cst.FACTION_BROBNAR, nb: 0};
+  factionLogosObject: any = {selected: false, faction: cst.FACTION_LOGOS, nb: 0};
+  factionMarsObject: any = {selected: false, faction: cst.FACTION_MARS, nb: 0};
+  factionSanctumObject: any = {selected: false, faction: cst.FACTION_SANCTUM, nb: 0};
+  factionShadowObject: any = {selected: false, faction: cst.FACTION_SHADOW, nb: 0};
+  factionUntamedObject: any = {selected: false, faction: cst.FACTION_UNTAMED, nb: 0};
 
 
   @ViewChild('lineCanvas') lineCanvas;
   barChart: any;
-  constructor(public loadingCtrl: LoadingController, 
+  constructor(public loadingCtrl: LoadingController,
     public afDB: AngularFireDatabase,
     private tools: ToolsProvider) {
-    this.getStats();    
+    this.getStats();
   }
 
-  ngOnInit(){    
+  ngOnInit(){
   }
 
   /**
    * Génération du graphique
    */
   onGenerateChart(){
-   
+
     let dataList = [];  // lignes de données
     let labelList = []; // labels de chaque ligne
     let backgroundColor = [];
@@ -60,7 +70,7 @@ export class StatisticPage implements OnInit{
     grd.addColorStop(1,"white");
 
     this.barChart = new Chart(this.lineCanvas.nativeElement, {
- 
+
       type: 'horizontalBar',
       data: {
           labels: labelList,//["Deck1", "Deck2", "Deck3", "Deck4", "Deck5", "Deck6", "Deck7"],
@@ -73,9 +83,9 @@ export class StatisticPage implements OnInit{
                   borderColor: borderColor,//'rgba(255,99,132,1)',//"rgba(0,0,0,5)",
                   borderCapStyle: 'butt',
                   borderDash: [],
-                  barThickness: 'flex',
+                  barThickness: 'flex', // largeur des barres. 'flex' = définition automatique de la largeur la plus appropriée
                   borderDashOffset: 0.0,
-                  borderJoinStyle: 'miter',
+                  borderJoinStyle: 'miter',// jointure de courbe (arrondie, plate ou pointue)
                   pointBorderColor: "rgba(0,0,0,1)",
                   pointBackgroundColor: "#fff",
                   pointBorderWidth: 0,
@@ -121,9 +131,115 @@ export class StatisticPage implements OnInit{
     }
   }
 
+  defineStat() {
+    let top10 = [];
+    this.factionWinRating = [];
+    this.factionBrobnarObject.nb = 0;
+    this.factionDisObject.nb = 0;
+    this.factionLogosObject.nb = 0;
+    this.factionMarsObject.nb = 0;
+    this.factionSanctumObject.nb = 0;
+    this.factionShadowObject.nb = 0;
+    this.factionUntamedObject.nb = 0;
+
+    if(this.decksTemp.length > 10) {
+      top10 = this.decksTemp.slice(0,10);
+      top10.forEach(deck => {
+        switch(deck.faction1.name) {
+          case cst.FACTION_BROBNAR.name :
+            this.factionBrobnarObject.nb++;
+          break;
+          case cst.FACTION_DIS.name :
+          this.factionDisObject.nb++;
+          break;
+          case cst.FACTION_LOGOS.name :
+          this.factionLogosObject.nb++;
+          break;
+          case cst.FACTION_MARS.name :
+          this.factionMarsObject.nb++;
+          break;
+          case cst.FACTION_SANCTUM.name :
+          this.factionSanctumObject.nb++;
+          break;
+          case cst.FACTION_SHADOW.name :
+          this.factionShadowObject.nb++;
+          break;
+          case cst.FACTION_UNTAMED.name :
+          this.factionUntamedObject.nb++;
+          break;
+          default: break;
+        }
+        switch(deck.faction2.name) {
+          case cst.FACTION_BROBNAR.name :
+            this.factionBrobnarObject.nb++;
+          break;
+          case cst.FACTION_DIS.name :
+          this.factionDisObject.nb++;
+          break;
+          case cst.FACTION_LOGOS.name :
+          this.factionLogosObject.nb++;
+          break;
+          case cst.FACTION_MARS.name :
+          this.factionMarsObject.nb++;
+          break;
+          case cst.FACTION_SANCTUM.name :
+          this.factionSanctumObject.nb++;
+          break;
+          case cst.FACTION_SHADOW.name :
+          this.factionShadowObject.nb++;
+          break;
+          case cst.FACTION_UNTAMED.name :
+          this.factionUntamedObject.nb++;
+          break;
+          default: break;
+        }
+        switch(deck.faction3.name) {
+          case cst.FACTION_BROBNAR.name :
+            this.factionBrobnarObject.nb++;
+          break;
+          case cst.FACTION_DIS.name :
+          this.factionDisObject.nb++;
+          break;
+          case cst.FACTION_LOGOS.name :
+          this.factionLogosObject.nb++;
+          break;
+          case cst.FACTION_MARS.name :
+          this.factionMarsObject.nb++;
+          break;
+          case cst.FACTION_SANCTUM.name :
+          this.factionSanctumObject.nb++;
+          break;
+          case cst.FACTION_SHADOW.name :
+          this.factionShadowObject.nb++;
+          break;
+          case cst.FACTION_UNTAMED.name :
+          this.factionUntamedObject.nb++;
+          break;
+          default: break;
+        }
+      });
+      const cptBrobnar = ((this.factionBrobnarObject.nb / 10) * 100).toFixed(0);
+      const cptDis = ((this.factionDisObject.nb / 10) * 100).toFixed(0);
+      const cptLogos = ((this.factionLogosObject.nb / 10) * 100).toFixed(0);
+      const cptSanctum = ((this.factionSanctumObject.nb / 10) * 100).toFixed(0);
+      const cptShadow = ((this.factionShadowObject.nb / 10) * 100).toFixed(0);
+      const cptMars = ((this.factionMarsObject.nb / 10) * 100).toFixed(0);
+      const cptUntamed = ((this.factionUntamedObject.nb / 10) * 100).toFixed(0);
+      this.factionWinRating.push({res: cptBrobnar, logo: this.factionBrobnarObject.faction});
+      this.factionWinRating.push({res: cptDis, logo: this.factionDisObject.faction});
+      this.factionWinRating.push({res: cptLogos, logo: this.factionLogosObject.faction});
+      this.factionWinRating.push({res: cptMars, logo: this.factionMarsObject.faction});
+      this.factionWinRating.push({res: cptSanctum, logo: this.factionSanctumObject.faction});
+      this.factionWinRating.push({res: cptShadow, logo: this.factionShadowObject.faction});
+      this.factionWinRating.push({res: cptUntamed, logo: this.factionUntamedObject.faction});
+      this.factionWinRating.sort(this.tools.predicateBy("res")).reverse();
+
+    }
+  }
+
   /**
    * Deck filtering
-   * @param ev 
+   * @param ev
    */
   filteringItem(ev){
     // Texte de recherche
@@ -132,6 +248,7 @@ export class StatisticPage implements OnInit{
     // Si texte vide, afficher tous les decks
     if (!searchText || searchText.trim() === '' || searchText === 'Tous') {
       console.log("filtering : tous");
+      this.filteringInProcess = false;
       // All decks
       this.decksObservable = this.deckList.valueChanges().map(items => items.sort(this.tools.predicateBy('win')).reverse());
       this.decksObservable.subscribe((res) => {
@@ -140,7 +257,8 @@ export class StatisticPage implements OnInit{
       })
       return;
     }
-    
+
+    this.filteringInProcess = true;
     // Filtre sur un deck spécifique
     this.decksObservable = this.deckList.valueChanges().map(items => items.filter((deck) => {
       if (deck.deckName.toLowerCase().indexOf(searchText.toLowerCase()) > -1) {
@@ -158,7 +276,7 @@ export class StatisticPage implements OnInit{
 
   /**
    * Calcul des stats pour un deck
-   * @param deckDataset 
+   * @param deckDataset
    */
   calculeStat(deckDataset){
     if(deckDataset){
@@ -168,7 +286,7 @@ export class StatisticPage implements OnInit{
           let pourcentWin = 0;
           let pourcentLoose = 0;
           let pourcentDraw = 0;
-        
+
           if(deckDataset[i].nbGames > 0){
             pourcentWin = deckDataset[i].win * (100/deckDataset[i].nbGames);
             pourcentLoose = deckDataset[i].loose * (100/deckDataset[i].nbGames);
@@ -177,7 +295,7 @@ export class StatisticPage implements OnInit{
             deckDataset[i]['pourcentLoose'] = pourcentLoose.toFixed(0);
             deckDataset[i]['pourcentDraw'] = pourcentDraw.toFixed(0);
           }
-        
+
           this.decksTemp.push({
             player: deckDataset[i].player,
             deckName: deckDataset[i].deckName,
@@ -187,16 +305,22 @@ export class StatisticPage implements OnInit{
             nbGames: deckDataset[i].nbGames,
             faction1: deckDataset[i].faction1,
             faction2: deckDataset[i].faction2,
-            faction3: deckDataset[i].faction3, 
+            faction3: deckDataset[i].faction3,
             pourcentWin: pourcentWin.toFixed(0),
             pourcentLoose: pourcentLoose.toFixed(0),
             pourcentDraw: pourcentDraw.toFixed(0)
           })
         }
-        
+
       }
       this.decksTemp.sort(this.tools.predicateBy2('pourcentWin')).reverse();
-      this.onGenerateChart();
+
+      // afficher le graphique uniquement si on est sur le filtre "Tous"
+      if(!this.filteringInProcess) {
+        this.onGenerateChart();
+      }
+
+      this.defineStat();
     }
   }
 
